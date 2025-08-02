e470fe4a0e21fe46894e2f20e1ff23da69fd04ca
Renusree T <kanchanatharani@gmail.com>
2025-08-02 22:33:33 +0530

    Create square pattern.java

---
+++ square pattern.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s = new Scanner(System.in);
+        int n,row,col;
+        n = s.nextInt();
+        for(row=1;row<=n;row++){
+            for(col=1;col<=n;col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
