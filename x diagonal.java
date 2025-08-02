7ccc82cdaa64251e43ea9ce145e55f346e1436b5
Renusree T <kanchanatharani@gmail.com>
2025-08-02 23:18:31 +0530

    Create x diagonal.java

---
+++ x diagonal.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s = new Scanner(System.in);
+        int n,row,col;
+        n = s.nextInt();
+        for(row=1;row<=2*n-1;row++){
+            for(col=1;col<=2*n-1;col++){
+                if(row==col||row+col==2*n){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
