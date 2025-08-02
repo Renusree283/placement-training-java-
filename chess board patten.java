98acec984bb6f2437c88a129e32a081844e5a146
Renusree T <kanchanatharani@gmail.com>
2025-08-02 22:35:02 +0530

    Create chess board patten.java

---
+++ chess board patten.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s = new Scanner(System.in);
+        int n,row,col;
+        n = s.nextInt();
+        for(row=1;row<=n;row++){
+            for(col=1;col<=n;col++){
+                if((row+col)%2 == 0)
+                    System.out.print("1");
+                else
+                    System.out.print("0");
+            }
+            System.out.println();
+        }
+    }
+}
