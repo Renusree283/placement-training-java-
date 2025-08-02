a76bb50fbb64da2641b89e6bac44594ab548e712
Renusree T <kanchanatharani@gmail.com>
2025-08-02 22:57:28 +0530

    Create rombus.java

---
+++ rombus.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s = new Scanner(System.in);
+        int n,row,space,star;
+        n = s.nextInt();
+        for(row = 1;row<=n;row++){
+            for(space=1;space<=n-row;space++){
+                System.out.print(" ");
+            }
+            for(star=1;star<=n;star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
