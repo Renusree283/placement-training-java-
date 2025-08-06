dc557e909cdc51ea8503dc0bbfa5a8c995a08d99
Renusree T <kanchanatharani@gmail.com>
2025-08-06 21:26:25 +0530

    Create replace every element in the array with the sum of its right side elements.java

---
+++ replace every element in the array with the sum of its right side elements.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int sum=0;
+        int a=sc.nextInt();
+        int b[]=new int[a];
+        for(int i=0;i<a;i++)
+        {
+            b[i]=sc.nextInt();
+            sum=sum+b[i]; 
+        }
+        for(int i=0;i<a;i++)
+        {
+            sum=sum-b[i];
+            System.out.print(sum+ " ");
+        }
+    }
+}
