a5552dbb154f08f3167ae4ce16ab5d33570050c6
Renusree T <kanchanatharani@gmail.com>
2025-08-06 21:08:27 +0530

    Create the sum of left side element.java

---
+++ the sum of left side element.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int sum=0;
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b[]=new int[a];
+        for(int i=0;i<a;i++)
+        {
+            System.out.print(sum+" ");
+            b[i]=sc.nextInt();
+            sum=sum+b[i]; 
+        }
+    }
+}
