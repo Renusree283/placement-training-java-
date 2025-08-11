ee1f63e4e10372eeebccf163077d18ceef00b3e7
Renusree T <kanchanatharani@gmail.com>
2025-08-11 22:10:55 +0530

    Create print the product with a precision of 2 decimal places .java

---
+++ print the product with a precision of 2 decimal places .java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s = new Scanner(System.in);
+        float a = s.nextFloat();
+        float b = s.nextFloat();
+        System.out.printf("%.2f",a*b);
+    }
+}
