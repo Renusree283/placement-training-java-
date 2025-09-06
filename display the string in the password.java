57f9c27d35bce1dd4eb1f678cd10a44ebe6fdeda
Renusree T <kanchanatharani@gmail.com>
2025-09-06 23:51:33 +0530

    Create  display the string in the password.java
    Write a program to accept the string from the user and display the string in the password format without making any change in the input array

---
+++ display the string in the password.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s = new Scanner(System.in);
+        String sc = s.nextLine();
+        for(int i=0;i<sc.length();i++){
+            System.out.print("*");
+        }
+    }
+}
