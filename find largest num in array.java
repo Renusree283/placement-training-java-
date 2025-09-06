d33a335b273ce1bdef2cad2cac5e58cc643a86ff
Renusree T <kanchanatharani@gmail.com>
2025-09-06 23:01:58 +0530

    Create find largest num in array.java
    Write a program to accept the array elements from the user and find the largest element in the array.

---
+++ find largest num in array.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s = new Scanner(System.in);
+        int n,i;
+        n = s.nextInt();
+        int a[] = new int[n];
+        for(i=0;i<n;i++){
+            a[i] = s.nextInt();
+        }
+        Arrays.sort(a);
+        System.out.print(a[n-1]);
+    }
+}
